1f97bc5efc76f29630a3c902b35b8e85fcb62394
pavelbannov <pavel.bannov@onlyoffice.com>
2023-10-03 18:14:05 +0300

    fixed migration and test

--- createMigrations.bat
+++ createMigrations.bat
@@ -1,6 +1,6 @@
 @echo "MIGRATIONS"
 @echo off
 
-PUSHD %~dp0..\common\Tools\ASC.Migration.Creator
+PUSHD %~dp0..\server\common\Tools\ASC.Migration.Creator
 dotnet run --project ASC.Migration.Creator.csproj
 pause
