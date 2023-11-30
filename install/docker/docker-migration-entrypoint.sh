d86ac7473282f7c4a6b068fd76498751f762641b
Sergey Kirichenko <Sergey.Kirichenko@onlyoffice.com>
2023-11-30 11:13:37 +0300

    Add connection timout for confg migration_ruuners ConnectionString

--- install/docker/docker-migration-entrypoint.sh
+++ install/docker/docker-migration-entrypoint.sh
@@ -5,9 +5,10 @@ MYSQL_DATABASE=${MYSQL_DATABASE:-"onlyoffice"}
 MYSQL_USER=${MYSQL_USER:-"onlyoffice_user"}
 MYSQL_PASSWORD=${MYSQL_PASSWORD:-"onlyoffice_pass"}
 MIGRATION_TYPE=${MIGRATION_TYPE:-"STANDALONE"}
+CONECTION_TIMEOUT=${CONNECTION_TIMEOUT:-"300"}
 PARAMETERS="standalone=true"
 
-sed -i "s!\"ConnectionString\".*!\"ConnectionString\": \"Server=${MYSQL_HOST};Database=${MYSQL_DATABASE};User ID=${MYSQL_USER};Password=${MYSQL_PASSWORD}\",!g" ./appsettings.runner.json
+sed -i "s!\"ConnectionString\".*!\"ConnectionString\": \"Server=${MYSQL_HOST};Database=${MYSQL_DATABASE};User ID=${MYSQL_USER};Password=${MYSQL_PASSWORD};Connection Timeout=${CONECTION_TIMEOUT}\",!g" ./appsettings.runner.json
 
 if [[ ${MIGRATION_TYPE} == "SAAS" ]]
 then
