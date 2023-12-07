02f61e6d56ac120e2d646bd1d1e7c24ac32f78ef
Evgeniy Antonyuk <antonyuk.evgenyiy@onlyoffice.com>
2023-12-07 17:31:54 +0700

    Fix error on changed repository metadata

--- install/OneClickInstall/install-Debian.sh
+++ install/OneClickInstall/install-Debian.sh
@@ -113,8 +113,8 @@ if [ -z "${SKIP_HARDWARE_CHECK}" ]; then
    SKIP_HARDWARE_CHECK="false";
 fi
 
+apt-get update -y --allow-releaseinfo-change;
 if [ $(dpkg-query -W -f='${Status}' curl 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-  apt-get update;
   apt-get install -yq curl;
 fi
 
