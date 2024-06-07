570dc223e8325fe11a5a9f3b2d3f05f9f25be32b
Nasrullo Nurullaev <61620246+nasrullonurullaev@users.noreply.github.com>
2024-06-07 18:38:39 +0500

    fix Bug 68348 - Update MySQL to 8.0.37 (#253)

--- install/win/build-download-prereq.ps1
+++ install/win/build-download-prereq.ps1
@@ -151,14 +151,14 @@ $enterprise_prerequisites = @(
 
   @{  
     download_allways = $false; 
-    name = "mysql-connector-odbc-8.0.33-win32.msi";
-    link = "https://cdn.mysql.com/archives/mysql-connector-odbc-8.0/mysql-connector-odbc-8.0.33-win32.msi";
+    name = "mysql-connector-odbc-8.0.37-win32.msi";
+    link = "https://cdn.mysql.com/Downloads/Connector-ODBC/8.0/mysql-connector-odbc-8.0.37-win32.msi";
   }
 
   @{  
     download_allways = $false; 
-    name = "mysql-installer-community-8.0.33.0.msi";
-    link = "https://cdn.mysql.com/archives/mysql-installer/mysql-installer-community-8.0.33.0.msi";
+    name = "mysql-installer-community-8.0.37.0.msi";
+    link = "https://cdn.mysql.com/Downloads/MySQLInstaller/mysql-installer-community-8.0.37.0.msi";
   }
 
   @{  
