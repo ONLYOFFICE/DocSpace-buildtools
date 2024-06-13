2a6fd1449fd1e5c5e63ed3fddcb43a33d0f6f363
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2024-06-13 15:07:39 +0400

    Fix run run.translations.tests.bat on windows

--- run.translations.tests.bat
+++ run.translations.tests.bat
@@ -1,4 +1,5 @@
 PUSHD %~dp0\..
-set dir="%cd%"
+set dir=%cd%
 echo %dir%
-dotnet test %dir%\client\common\Tests\Frontend.Translations.Tests\Frontend.Translations.Tests.csproj --filter "TestCategory=Locales" -l:html --environment "BASE_DIR=%dir%" --results-directory "%dir%\TestsResults"
+dotnet test %dir%\client\common\Tests\Frontend.Translations.Tests\Frontend.Translations.Tests.csproj --filter "TestCategory=Locales" -l:html --environment "BASE_DIR=%dir%\client" --results-directory "%dir%\TestsResults"
+pause
