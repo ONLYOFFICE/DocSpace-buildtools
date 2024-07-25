73569d3a15e9108d93f873a152017df1224cd010
Evgeniy Antonyuk <antonyuk.evgenyiy@onlyoffice.com>
2024-07-25 15:47:02 +0300

    Creating and publishing an offline archive

---
+++ install/common/self-extracting.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+[ "$(id -u)" -ne 0 ] && { echo "To perform this action you must be logged in with root rights"; exit 1; }
+
+TEMP_DIR=$(mktemp -d)
+echo "Unpacking files to ${TEMP_DIR}..."
+tail -n +$(awk '/^__END_OF_SHELL_SCRIPT__$/{print NR + 1; exit 0;}' "$0") "$0" | tar xz -C "$TEMP_DIR"
+
+echo "Loading Docker images from offline_images.tgz..."
+docker load -i ${TEMP_DIR}/offline_images.tgz
+
+echo "Run the install-Docker.sh script..."
+chmod +x ${TEMP_DIR}/install-Docker.sh
+${TEMP_DIR}/install-Docker.sh
+
+rm -rf "${TEMP_DIR}"
+
+exit 0
+
+__END_OF_SHELL_SCRIPT__
