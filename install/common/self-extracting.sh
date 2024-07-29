f24bed5c3340425d59ba1e98327867c4968d5cad
Evgeniy Antonyuk <antonyuk.evgenyiy@onlyoffice.com>
2024-07-29 16:52:34 +0300

    fix Bug 69111 - Add an offline installation option for docker supply

---
+++ install/common/self-extracting.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+[ "$(id -u)" -ne 0 ] && { echo "To perform this action you must be logged in with root rights"; exit 1; }
+
+TEMP_DIR=$(mktemp -d)
+
+trap 'echo "Cleaning up temporary files..."; rm -rf "${TEMP_DIR}"' EXIT
+
+! type docker &> /dev/null && { echo "docker not installed"; exit 1; }
+! type docker-compose &> /dev/null && { echo "docker-compose not installed"; exit 1; }
+
+echo "Extracting docker images to ${TEMP_DIR}..."
+tail -n +$(awk '/^__END_OF_SHELL_SCRIPT__$/{print NR + 1; exit 0;}' "$0") "$0" | tar x -C "${TEMP_DIR}"
+
+echo "Loading docker images..."
+docker load -i ${TEMP_DIR}/docker_images.tar.xz
+
+echo "Extracting OneClickInstall files to the current directory..."
+mv -f ${TEMP_DIR}/docker.tar.gz $(dirname "$0")/docker.tar.gz
+mv -f ${TEMP_DIR}/install-Docker.sh $(dirname "$0")/install-Docker.sh
+
+echo "Running the install-Docker.sh script..."
+chmod +x $(dirname "$0")/install-Docker.sh
+$(dirname "$0")/install-Docker.sh
+
+exit 0
+
+__END_OF_SHELL_SCRIPT__
