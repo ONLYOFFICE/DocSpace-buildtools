00163fe863aaed21a18b85fff3c5dbdd83e0c16a
Sergey Kirichenko <Sergey.Kirichenko@onlyoffice.com>
2024-10-03 17:34:40 +0300

    Add EE config

--- install/docker/docker-entrypoint.py
+++ install/docker/docker-entrypoint.py
@@ -177,6 +177,7 @@ updateJsonData(jsonData,"$.debug-info.enabled", DEBUG_INFO)
 updateJsonData(jsonData,"$.web.samesite", SAMESITE)
 if INSTALLATION_TYPE == "ENTERPRISE":
     updateJsonData(jsonData, "$.license.file.path", "/app/onlyoffice/data/license.lic")
+    ENV_EXTENSION="ee"
 
 ip_address = netifaces.ifaddresses('eth0').get(netifaces.AF_INET)[0].get('addr')
 netmask = netifaces.ifaddresses('eth0').get(netifaces.AF_INET)[0].get('netmask')
