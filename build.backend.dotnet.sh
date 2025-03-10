a29a47838d01fc23ceaebc365eb852c8a79d009e
alexeybannov <alexey.bannov@onlyoffice.com>
2025-03-10 14:26:31 +0300

    macos: change RunAtLoad to false

--- build.backend.dotnet.sh
+++ build.backend.dotnet.sh
@@ -8,16 +8,10 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR"
 
 if [ $? -eq 0 ]; then
-    bash start/stop.sh nopause
+    bash start/stop.sh
     dotnet build ../server/asc.web.slnf /fl1 "/flp1:logfile=asc.web.log;verbosity=normal"
 fi
 
 echo
 
-bash start/stop.sh nopause
-
-echo
-
-if [ "$1" != "nopause" ]; then
-    read -p "Press Enter to continue..."
-fi
+bash start/start.sh
