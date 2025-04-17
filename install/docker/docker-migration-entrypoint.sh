efccca3a8e60590998d9e2ee26dcd7df95169889
Sergey Kirichenko <Sergey.Kirichenko@onlyoffice.com>
2025-04-17 12:58:09 +0300

    Add customization timeout for migration

--- install/docker/docker-migration-entrypoint.sh
+++ install/docker/docker-migration-entrypoint.sh
@@ -5,10 +5,11 @@ MYSQL_PORT=${MYSQL_PORT:-"3306"}
 MYSQL_DATABASE=${MYSQL_DATABASE:-"onlyoffice"}
 MYSQL_USER=${MYSQL_USER:-"onlyoffice_user"}
 MYSQL_PASSWORD=${MYSQL_PASSWORD:-"onlyoffice_pass"}
+COMMAND_TIMEOUT=${COMMAND_TIMEOUT:-"100"}
 MIGRATION_TYPE=${MIGRATION_TYPE:-"STANDALONE"}
 PARAMETERS="standalone=true"
 
-sed -i "s!\"ConnectionString\".*!\"ConnectionString\": \"Server=${MYSQL_HOST};Port=${MYSQL_PORT};Database=${MYSQL_DATABASE};User ID=${MYSQL_USER};Password=${MYSQL_PASSWORD}\",!g" ./appsettings.runner.json
+sed -i "s!\"ConnectionString\".*!\"ConnectionString\": \"Server=${MYSQL_HOST};Port=${MYSQL_PORT};Database=${MYSQL_DATABASE};User ID=${MYSQL_USER};Password=${MYSQL_PASSWORD};Command Timeout=${COMMAND_TIMEOUT}\"!g" ./appsettings.runner.json
 
 if [[ ${MIGRATION_TYPE} == "SAAS" ]]
 then
