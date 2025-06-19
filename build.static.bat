6851d81173ae4dcc53ba03c9949ae3df215e2217
Alexey Safronov <alexey.safronov@onlyoffice.com>
2025-06-19 17:51:35 +0400

    Fix build.static.bat

--- build.static.bat
+++ build.static.bat
@@ -13,23 +13,19 @@ cd client
 REM call pnpm install
 call pnpm install
 
-REM call pnpm build
-call pnpm nx run-many -t build -p @docspace/client @docspace/login @docspace/doceditor @docspace/management @docspace/sdk --parallel=5
-
-REM call pnpm deploy
-call pnpm nx run-many -t deploy -p @docspace/client @docspace/login @docspace/doceditor @docspace/management @docspace/sdk --parallel=5
+REM call pnpm deploy (build will be called when necessary)
+call pnpm run deploy
 
 cd ..
 
 REM copy nginx configurations to deploy folder
 xcopy buildtools\config\nginx\onlyoffice.conf publish\nginx\ /E /R /Y
-powershell -Command "(gc publish\nginx\onlyoffice.conf) -replace '#', '' | Out-File -encoding ASCII publish\nginx\onlyoffice.conf"
 
 xcopy buildtools\config\nginx\sites-enabled\* publish\nginx\sites-enabled\ /E /R /Y
+xcopy buildtools\config\nginx\html\* publish\nginx\html\ /E /R /Y
 
 REM fix paths
 powershell -Command "(gc publish\nginx\sites-enabled\onlyoffice-client.conf) -replace 'ROOTPATH', '%parentFolder%\publish\web\client' -replace '\\', '/' | Out-File -encoding ASCII publish\nginx\sites-enabled\onlyoffice-client.conf"
-powershell -Command "(gc publish\nginx\sites-enabled\onlyoffice-management.conf) -replace 'ROOTPATH', '%parentFolder%\publish\web\management' -replace '\\', '/' | Out-File -encoding ASCII publish\nginx\sites-enabled\onlyoffice-management.conf"
 
 REM restart nginx
 echo service nginx stop
