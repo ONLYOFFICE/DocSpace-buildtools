eea55b52edec483299c046a66f0e1d7b048393e5
Andrei Panov <andrew.panov@onlyoffice.com>
2025-08-14 12:06:31 +0300

    fix: fix telegram subscription name

--- run/Telegram.bat
+++ run/Telegram.bat
@@ -1,4 +1,4 @@
 @echo off
 
 PUSHD %~dp0..\..
-set servicepath=%cd%\server\common\services\ASC.TelegramService\bin\Debug\ASC.TelegramService.exe urls=http://0.0.0.0:5075 $STORAGE_ROOT=%cd%\Data log:dir=%cd%\Logs log:name=telegram pathToConf=%cd%\buildtools\config core:products:folder=%cd%\server\products core:eventBus:subscriptionClientName=asc_event_bus_notify_queue
+set servicepath=%cd%\server\common\services\ASC.TelegramService\bin\Debug\ASC.TelegramService.exe urls=http://0.0.0.0:5075 $STORAGE_ROOT=%cd%\Data log:dir=%cd%\Logs log:name=telegram pathToConf=%cd%\buildtools\config core:products:folder=%cd%\server\products core:eventBus:subscriptionClientName=asc_event_bus_telegram_queue
