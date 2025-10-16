eeed157c228f1cde7ea414026b4c52f9fc76c360
Nasrullo Nurullaev <nasrullo.nurullaev@onlyoffice.com>
2025-10-16 20:01:39 +0500

    Fix issue with building frontend part in win

--- install/win/frontend-build.bat
+++ install/win/frontend-build.bat
@@ -4,32 +4,12 @@ echo ######################
 echo #   build frontend   #
 echo ######################
 
-set DEBUG_INFO=%~2
+%sed% -i "s/^; node-linker=hoisted/node-linker=hoisted/" client\.npmrc
 
 pushd %~s1
 
   call pnpm install
 
-  call node common\scripts\before-build.js
-
-  set TS_ERRORS_IGNORE=true
-
-  call pnpm nx build @docspace/client --env lint=false
-  call pnpm nx deploy @docspace/client 
-
-  call pnpm nx build @docspace/login
-  call pnpm nx deploy @docspace/login
-
-  call pnpm nx build @docspace/doceditor
-  call pnpm nx deploy @docspace/doceditor
-
-  call pnpm nx build @docspace/management
-  call pnpm nx deploy @docspace/management
-
-  call pnpm nx build @docspace/sdk
-  call pnpm nx deploy @docspace/sdk
-
-  xcopy /E /I /Y public "..\publish\web\public\"
-  call node common\scripts\minify-common-locales.js
+  call pnpm run deploy
 
 popd
