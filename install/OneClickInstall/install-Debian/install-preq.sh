8dddfe6ee8693584cf962b4271a59a533331d993
Evgeniy Antonyuk <antonyuk.evgenyiy@onlyoffice.com>
2025-10-23 15:50:18 +0200

    fix Bug 77825 - MySQL repo GPG key expired

--- install/OneClickInstall/install-Debian/install-preq.sh
+++ install/OneClickInstall/install-Debian/install-preq.sh
@@ -95,6 +95,11 @@ elif dpkg -l | grep -q "mysql-apt-config" && [ "$(apt-cache policy mysql-apt-con
 	rm -f "${MYSQL_PACKAGE_NAME}"
 fi
 
+# fix Bug 77825 - MySQL repo GPG key expired
+if [ -f "/etc/apt/sources.list.d/mysql.list" ]; then
+	sed -E -i 's/\[trusted=(no|yes) /\[trusted=yes /; /\[trusted=yes /! s/\[signed-by=(.*)\]/[trusted=yes signed-by=\1]/' /etc/apt/sources.list.d/mysql.list
+fi
+
 if [ "$DIST" = "ubuntu" ]; then	
 	curl -fsSL https://packages.redis.io/gpg | gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/redis.gpg --import
 	echo "deb [signed-by=/usr/share/keyrings/redis.gpg] https://packages.redis.io/deb ${DISTRIB_CODENAME} main" | tee /etc/apt/sources.list.d/redis.list
