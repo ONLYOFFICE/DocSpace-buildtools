87077ed5b5a10499158d65c7a58a426f5afbef23
Evgeniy Antonyuk <antonyuk.evgenyiy@onlyoffice.com>
2025-10-30 23:45:41 +0200

    Fix the plugin build issue

--- install/common/plugins-build.sh
+++ install/common/plugins-build.sh
@@ -6,7 +6,7 @@ MAX_JOBS=${MAX_JOBS:-4}
 
 find "$SRC_PATH" -mindepth 1 -maxdepth 1 -type d \( -exec test -f "{}/yarn.lock" \; -o -exec test -f "{}/package.json" \; \) -print | \
 xargs -P "$MAX_JOBS" -I{} bash -c '
-  PLUGIN_DIR="$1"; PLUGIN_NAME=$(basename "$PLUGIN_DIR")
+  PLUGIN_DIR="$1"; SRC_PATH="$2"; PLUGIN_NAME=$(basename "$PLUGIN_DIR")
   echo "=== Building plugin: $PLUGIN_NAME ==="
   cd "$PLUGIN_DIR" || { echo "::error:: Cannot cd to $PLUGIN_DIR"; exit 0; }
 
@@ -15,4 +15,4 @@ xargs -P "$MAX_JOBS" -I{} bash -c '
 
   mkdir -p "$SRC_PATH/publish/$PLUGIN_NAME"
   unzip -qo "$PLUGIN_DIR/dist/plugin.zip" -d "$SRC_PATH/publish/$PLUGIN_NAME" || echo "::error:: No plugin.zip found for $PLUGIN_NAME"
-' _ {}
+' _ {} "$SRC_PATH"
