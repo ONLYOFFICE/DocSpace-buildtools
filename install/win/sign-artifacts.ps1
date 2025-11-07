4bb36f9992b65bbc1de8e7ab5c9c210142bbda3a
Nasrullo Nurullaev <nasrullo.nurullaev@onlyoffice.com>
2025-11-07 23:54:50 +0500

    Add signing all windows artifacts

---
+++ install/win/sign-artifacts.ps1
@@ -0,0 +1,19 @@
+# Path to signtool
+$signtool = "C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\signtool.exe"
+
+# Path to folder
+$folderPath = Join-Path (Get-Location) "Files"
+$timestampUrl = "http://timestamp.digicert.com"
+
+Write-Host "Folder path: $folderPath"
+
+# Find files
+$files = Get-ChildItem -Path $folderPath -Recurse -File | Where-Object { $_.Extension -in '.exe','.dll','.pdb' }
+Write-Host "Found files count: $($files.Count)"
+
+foreach ($file in $files) {
+    Write-Host "Signing: $($file.FullName)"
+    & "$signtool" sign /f "$env:onlyoffice_codesign_path" /p "$env:onlyoffice_codesign_password" /tr "$timestampUrl" /td SHA256 /fd SHA256 "$($file.FullName)" | Out-Null
+}
+
+Write-Host "Signing completed."
