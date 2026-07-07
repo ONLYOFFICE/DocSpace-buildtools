39af4c3c513ee0a2a4245e5f012ed8fd8487ced0
Evgeniy Antonyuk <antonyuk.evgenyiy@onlyoffice.com>
2026-07-07 15:34:03 +0200

    Refactor DocumentServer package upgrade logic

--- install/OneClickInstall/install-Debian/install-app.sh
+++ install/OneClickInstall/install-Debian/install-app.sh
@@ -64,21 +64,18 @@ setup_postgres_db() {
 		su - postgres -s /bin/bash -c "psql -c \"CREATE DATABASE ${DS_DB_NAME} OWNER ${DS_DB_USER};\""
 	fi
 
-	echo "${package_sysname}"-documentserver "$DS_COMMON_NAME"/db-pwd select "$DS_DB_PWD" | debconf-set-selections
-	echo "${package_sysname}"-documentserver "$DS_COMMON_NAME"/db-user select "$DS_DB_USER" | debconf-set-selections
-	echo "${package_sysname}"-documentserver "$DS_COMMON_NAME"/db-name select "$DS_DB_NAME" | debconf-set-selections
+	echo "${ds_pkg_name}" "$DS_COMMON_NAME"/db-pwd select "$DS_DB_PWD" | debconf-set-selections
+	echo "${ds_pkg_name}" "$DS_COMMON_NAME"/db-user select "$DS_DB_USER" | debconf-set-selections
+	echo "${ds_pkg_name}" "$DS_COMMON_NAME"/db-name select "$DS_DB_NAME" | debconf-set-selections
 }
 
 if [ "$UPDATE" = "true" ] && [ "$DOCUMENT_SERVER_INSTALLED" = "true" ]; then
-	ds_pkg_installed_name=$(dpkg -l | grep "${package_sysname}"-documentserver | tail -n1 | awk '{print $2}')
-	if [ -n "${ds_pkg_installed_name}" ] && [ "${ds_pkg_installed_name}" != "${ds_pkg_name}" ]; then
-		[ "$INSTALLATION_TYPE" != "COMMUNITY" ] && [ "${ds_pkg_installed_name}" = "${package_sysname}-documentserver" ] && setup_postgres_db
-		debconf-get-selections | grep ^"${ds_pkg_installed_name}" | sed s/"${ds_pkg_installed_name}"/"${ds_pkg_name}"/g | debconf-set-selections
-		DEBIAN_FRONTEND=noninteractive apt-get purge -yq "${ds_pkg_installed_name}"
-		# Temporary kill orphaned nginx workers blocking port after purge
-		nginx -s reload 2>/dev/null || pkill -x nginx 2>/dev/null || true
-		apt-get install -yq "${ds_pkg_name}"
-		RECONFIGURE_PRODUCT="true"
+	ds_pkg_installed_name=$(dpkg -l | awk -v pkg="${package_sysname}-documentserver" '$1=="ii" && index($2,pkg)==1{print $2}' | tail -n1)
+    if [ -z "${ds_pkg_installed_name}" ]; then
+        RECONFIGURE_PRODUCT="true"; DOCUMENT_SERVER_INSTALLED="false"
+	elif [ -n "${ds_pkg_installed_name}" ] && [ "${ds_pkg_installed_name}" != "${ds_pkg_name}" ]; then
+		apt-get remove -yq "${ds_pkg_installed_name}"
+		RECONFIGURE_PRODUCT="true"; DOCUMENT_SERVER_INSTALLED="false"
 	else
 		apt-get install -y --only-upgrade "${ds_pkg_name}"
 	fi
@@ -90,10 +87,10 @@ if [ "$DOCUMENT_SERVER_INSTALLED" = "false" ]; then
 	DS_JWT_SECRET=${DS_JWT_SECRET:-$(cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 32)}
 	DS_JWT_HEADER=${DS_JWT_HEADER:-AuthorizationJwt}
 
-	echo "${package_sysname}"-documentserver "$DS_COMMON_NAME"/ds-port select "$DS_PORT" | debconf-set-selections
-	echo "${package_sysname}"-documentserver "$DS_COMMON_NAME"/jwt-enabled select "${DS_JWT_ENABLED}" | debconf-set-selections
-	echo "${package_sysname}"-documentserver "$DS_COMMON_NAME"/jwt-secret select "${DS_JWT_SECRET}" | debconf-set-selections
-	echo "${package_sysname}"-documentserver "$DS_COMMON_NAME"/jwt-header select "${DS_JWT_HEADER}" | debconf-set-selections
+	echo "${ds_pkg_name}" "$DS_COMMON_NAME"/ds-port select "$DS_PORT" | debconf-set-selections
+	echo "${ds_pkg_name}" "$DS_COMMON_NAME"/jwt-enabled select "${DS_JWT_ENABLED}" | debconf-set-selections
+	echo "${ds_pkg_name}" "$DS_COMMON_NAME"/jwt-secret select "${DS_JWT_SECRET}" | debconf-set-selections
+	echo "${ds_pkg_name}" "$DS_COMMON_NAME"/jwt-header select "${DS_JWT_HEADER}" | debconf-set-selections
 
 	[ "$INSTALLATION_TYPE" != "COMMUNITY" ] && setup_postgres_db
 
