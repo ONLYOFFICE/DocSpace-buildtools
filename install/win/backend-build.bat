17a34f1bfa0607956c989ad0d915381aeb4f5f42
Nasrullo Nurullaev <nasrullo.nurullaev@onlyoffice.com>
2026-09-15 23:21:03 +0500

    Exclude AI chat dev dependencies from Windows installer

--- install/win/backend-build.bat
+++ install/win/backend-build.bat
@@ -19,6 +19,7 @@ pushd %~1
   echo "== Build ASC.AI.Chat =="
   pushd common\ASC.AI.Chat
     call yarn install --frozen-lockfile
+    call yarn workspaces focus --production
   popd
 
   echo "== Build ASC.SsoAuth =="
